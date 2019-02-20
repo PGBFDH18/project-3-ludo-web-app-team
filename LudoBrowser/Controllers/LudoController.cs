1f09759c5455ff12826f7aa3dfe865d80d146435
ThisRocky <44053635+ThisRocky@users.noreply.github.com>
2019-02-20 12:41:36 +0100

    LastPartController
    Last part of the controller to make the game fully playable has been added. Model validation still left

--- LudoBrowser/Controllers/LudoController.cs
+++ LudoBrowser/Controllers/LudoController.cs
@@ -81,8 +81,101 @@ namespace LudoBrowser.Controllers
             return View();
         }
 
+        [HttpGet("{gameId}")]
+        public string addplayer()
+        {
+
+            return null;
+        }
+        [Route("/delete/{gameID}")]
+        public IActionResult delete(int gameID)
+        {
+            var getGameInformation = new RestRequest("/Ludo/" + gameID, Method.DELETE);
+
+            IRestResponse<GameModel> ludoGameResponse = Client.Execute<GameModel>(getGameInformation);
+
+            return Redirect("/");
+        }
+        [HttpGet("/startgame/{gameID}")]
+        public IActionResult startgame(int gameID, string name1, string name2, string name3, string name4)
+        {
+
+
+            //List<string> PlayerNames = new List<string>() { name1, name2, name3, name4 };   
+            if (name1 != null)
+            {
+
+                LudoPlayer player = new LudoPlayer()
+                {
+                    Color = "red",
+                    Id = 0,
+                    Name = name1
+                };
+                addUser(gameID, player);
+            }
+            if (name2 != null)
+            {
+
+                LudoPlayer player = new LudoPlayer()
+                {
+                    Color = "yellow",
+                    Id = 1,
+                    Name = name2
+                };
+                addUser(gameID, player);
+            }
+            if (name3 != null)
+            {
+
+                LudoPlayer player = new LudoPlayer()
+                {
+                    Color = "green",
+                    Id = 2,
+                    Name = name3
+                };
+                addUser(gameID, player);
+            }
+            if (name4 != null)
+            {
+                LudoPlayer player = new LudoPlayer()
+                {
+                    Color = "blue",
+                    Id = 3,
+                    Name = name4
+                };
+                addUser(gameID, player);
+            }
+            var request = new RestRequest("/ludo/" + gameID + "/players", Method.GET);
+
+            IRestResponse<List<LudoPlayer>> PlayersListResponse = Client.Execute<List<LudoPlayer>>(request);
+            List<LudoPlayer> Playerlist = PlayersListResponse.Data;
+            Playerlist.ToString();
+            ViewBag.gameID = gameID;
+            return View(Playerlist);
+        }
+        private void addUser(int gameID, LudoPlayer player)
+        {
+            var request = new RestRequest("/ludo/" + gameID + "/players", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(player);
+            IRestResponse addPlayer = Client.Execute(request);
+            addPlayer.ToString();
+        }
+        [HttpGet("/move/{gameID}")]
+        public void PlayGame(int gameID, int numberoffield, int pieceid, int playerid)
+        {
+            var request = new RestRequest("/ludo/" + gameID, Method.PUT);
+            MovePiece movePiece = new MovePiece()
+            {
+                NumberOfFields = 6,
+                PieceId = 0,
+                PlayerId = 0,
+            };
+            request.RequestFormat = DataFormat.Json;
+            request.AddBody(movePiece);
+            IRestResponse restResponse = Client.Execute(request);
+
+        }
     }
 
 }
-
-
---
+++ LudoBrowser/Models/MovePiece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoBrowser.Models
+{
+    public class MovePiece
+    {
+        public int PlayerId { get; set; }
+        public int PieceId { get; set; }
+        public int NumberOfFields { get; set; }
+    }
+}
